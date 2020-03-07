72f714bbc4f5b4e7d6b33743a3a0536357e5ba2e
xmc <xmc409@126.com>
2020-03-07 15:10:17 +0800

    3

---
+++ test/src/test/Test3.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Test3 {
+	public static void main(String[] args) {
+		System.out.println("xxs222xx");
+		System.out.println("fail");
+		System.out.println("xxs222xx");System.out.println("xxs222xx");
+		System.out.println("xxs222xx");
+	}
+}
