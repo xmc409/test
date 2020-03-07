9939ffaac22fd60c7d3fdc44eeabb26adf4e7478
xmc <xmc409@126.com>
2020-03-07 15:25:40 +0800

    555

--- test/src/test/Test3.java
+++ test/src/test/Test3.java
@@ -4,7 +4,7 @@ public class Test3 {
 	public static void main(String[] args) {
 		System.out.println("xxs222xx");
 		System.out.println("fail");
-		System.out.println("xxs222xx");System.out.println("xxs222xx");
+		System.out.println("xxs555xx");System.out.println("xxs222xx");
 		System.out.println("xxs222xx");
 	}
 }
